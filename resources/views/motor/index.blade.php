ae0de9aed6526199704cd675ab305056ef85bf34
JosephWilliam1210 <jwilliam@users.noreply.github.com>
2020-11-05 08:05:41 -0800

    update file

--- resources/views/motor/index.blade.php
+++ resources/views/motor/index.blade.php
@@ -1,6 +1,27 @@
 @extends('layouts.app')
 @section('content')
     <div class="container" style="margin-top: 20px;">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/077067300_1567575292-vespa-4402984_1920.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/harga-vespa-primavera-sean-wotherspoon.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/piaggio-vespa-1255ed09ba4a5485.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
         <div class="row">
             <h1 class="col">List Motor</h1>
         </div>
